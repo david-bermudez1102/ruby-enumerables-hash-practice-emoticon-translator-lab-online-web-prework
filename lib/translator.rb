469723393a371ad6ca496bc80ae24718716e0d87
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-27 03:06:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,9 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path,emoticon)
-  load_library(file_path)
+  get_emoticons = load_library(file_path)
+  get_emoticon[:get_emoticon][emoticon]
+  
 end
 
 def get_english_meaning
