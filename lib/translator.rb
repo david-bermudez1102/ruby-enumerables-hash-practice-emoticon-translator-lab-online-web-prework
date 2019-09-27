f26361bda810b83e8b0a71d3acebad06853660bb
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-27 02:57:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
 
   hash = {:get_meaning=>{}, :get_emoticon=>{}}
   thing.each_with_object([]) { |(definition,emoticon),array| array = emoticon.to_s
-      hash[:get_meaning][emoticon[1]] = "Hi"
+      hash[:get_meaning][emoticon[1]] = definition
     }
 
   print hash
