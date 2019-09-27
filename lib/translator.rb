fba071d017981aad4195566b9d54d54846aa053b
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-27 03:01:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
   hash = {:get_meaning=>{}, :get_emoticon=>{}}
   thing.each { |definition,emoticon|
       hash[:get_meaning][emoticon[1]] = definition
-      hash[:get_meaning][emoticon[0]] = emoticon[1]
+      hash[:get_emoticon][emoticon[0]] = emoticon[1]
     }
 
   print hash
