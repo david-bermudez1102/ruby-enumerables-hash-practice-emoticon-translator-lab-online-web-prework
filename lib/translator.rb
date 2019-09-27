6fee5f21c3926b3a0d9f34751d0a348d0dceda51
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-27 02:56:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  require 'yaml'
+  thing = YAML.load_file(file_path)
+  hash = {:get_meaning=>{:japanese_emoticon=>thing['angel'][1]}, :get_emoticon=>{}}
+  print thing
+  hash
 end
 
 def get_japanese_emoticon
